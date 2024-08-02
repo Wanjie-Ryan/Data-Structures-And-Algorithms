cda92df5483c71e6f3694dd82c692061e081e60e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 18:49:34 +0300

    Tower of Hanoi implementation

---
+++ Tower of Hanoi/hanoi.js
@@ -0,0 +1,4 @@
+// should move the entire stack to the last rod, whilst obeying the rules:
+// 1. Only one disk maybe moved at a time.
+// 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or an empty rod
+// 3. No disk may be placed on top of a smaller disk.
