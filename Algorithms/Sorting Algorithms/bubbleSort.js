2fdb707da2d46f882024de87479fb29ba73ca809
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 17:33:50 +0300

    quick sort implementation

--- Algorithms/Sorting Algorithms/bubbleSort.js
+++ Algorithms/Sorting Algorithms/bubbleSort.js
@@ -1,32 +1,29 @@
 // Given an array of integers, sort the array (ascending or descending)
 // poor sorting algorithm, used as a stepping stone, to learn other algorithms
-// compare adjacent elements in the array and swap the positions if they are in the intended order. 
+// compare adjacent elements in the array and swap the positions if they are in the intended order.
 // Repeat the instruction as you step through each element in thee array
 // once you step through the whole array with no swaps, the array is sorted
 // the highest number is said to have bubbled to the top, hence the name bubble sort
 
-function BubbleSort(arr){
+function BubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
 
-    let swapped
-    do{
-      swapped = false
-        
-        for(var i =0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i+1]){
-                // so what is happening here is that, we are comparing 2 values, if the left value is greater than the right value, then we assign the left value to a variable called temp, the left element is left empty, we then assign the right element to the vaccated left element, and the then the right element has been left empty, we assign the temp to the right element
-                var temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i +1] = temp
-                swapped = true
-            }
-        }
-    }while(swapped)
-        // while swapped is true, keep running the for loop
-
-
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // so what is happening here is that, we are comparing 2 values, if the left value is greater than the right value, then we assign the left value to a variable called temp, the left element is left empty, we then assign the right element to the vaccated left element, and the then the right element has been left empty, we assign the temp to the right element
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  // while swapped is true, keep running the for loop
 }
 
-let arr = [8, 20, -2, 4, -6]
-BubbleSort(arr)
-console.log(arr)
+let arr = [8, 20, -2, 4, -6];
+BubbleSort(arr);
+console.log(arr);
 // the big o notation is O(n^2) as the are working with nested loops
--- Algorithms/Sorting Algorithms/quickSort.js
+++ Algorithms/Sorting Algorithms/quickSort.js
@@ -8,40 +8,31 @@
 // Repeat the process for the individual left and right arrays until you have an array of length 1 which is sorted by definition
 // Repeatedly concatenate the left array, the pivot and the right array to get the sorted array
 
-function QuickSort(arr){
-
-    if(arr.length < 2){
-        return arr;  // base case: an array of length 1 is already sorted.
-    }
-    // find the pivot of the array, which in our case is the last element of the array.
-    let pivot = arr[arr.length - 1];
-    // console.log(pivot);
-    // traverse the array and put the elements in a left and right array.
-    let left = [];
-    let right = []
-    // a for loop to traverse the array.
-    for(var i = 0; i< arr.length -1; i++){
-
-        // if the element is less than the pivot, it will be pushed to the left of the array, and if greater, it will be pushed to the right of the array.
-        if(arr[i] < pivot){
-            left.push(arr[i])
-        }
-        else{
-            right.push(arr[i])
-        }
-
-        // repeat the above process for the right and left sub array, and concatenate it with the pivot element
-        // this here is recusrion, and to exit a recusrion we need a base case
-        return[...QuickSort(left), pivot, ...QuickSort(right)]
-
-
-
+function QuickSort(arr) {
+  if (arr.length < 2) {
+    return arr; // base case: an array of length 1 is already sorted.
+  }
+  // find the pivot of the array, which in our case is the last element of the array.
+  let pivot = arr[arr.length - 1];
+  // console.log(pivot);
+  // traverse the array and put the elements in a left and right array.
+  let left = [];
+  let right = [];
+  // a for loop to traverse the array.
+  for (var i = 0; i < arr.length - 1; i++) {
+    // if the element is less than the pivot, it will be pushed to the left of the array, and if greater, it will be pushed to the right of the array.
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
 
-
-
+    // repeat the above process for the right and left sub array, and concatenate it with the pivot element
+    // this here is recusrion, and to exit a recusrion we need a base case
+    return [...QuickSort(left), pivot, ...QuickSort(right)];
+  }
 }
 
-let arr = [8, 20, -2, 4, -6]
-console.log(QuickSort(arr))
+let arr = [8, 20, -2, 4, -6];
+console.log(QuickSort(arr));
 // console.log(arr)
