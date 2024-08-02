1565ede2a114957702b5c015a8a3a80f41e52318
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 17:04:18 +0300

    quick sort implementation

--- Algorithms/Sorting Algorithms/quickSort.js
+++ Algorithms/Sorting Algorithms/quickSort.js
@@ -3,3 +3,7 @@
 // 2. Pick the last element as pivot
 // 3. Pick a random element as pivot
 // 4. Pick median as pivot
+
+// Traverse the array and put everything that is smaller than the pivot into a left array and everything that is greater than the pivot into a right array
+// Repeat the process for the individual left and right arrays until you have an array of length 1 which is sorted by definition
+// Repeatedly concatenate the left array, the pivot and the right array to get the sorted array
