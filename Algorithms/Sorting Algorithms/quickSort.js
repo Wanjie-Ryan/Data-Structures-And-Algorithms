3ea409c2c4393c59ae4e569dc38b250cfc6b9267
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 16:56:09 +0300

    quick sort implementation

---
+++ Algorithms/Sorting Algorithms/quickSort.js
@@ -0,0 +1,5 @@
+// you need to identify the pivot element in the array by the following ways:
+// 1. Pick the first element as pivot
+// 2. Pick the last element as pivot
+// 3. Pick a random element as pivot
+// 4. Pick median as pivot
