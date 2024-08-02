21e839cca529d75872ce4b9052d5e7a81a1bf675
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 15:59:47 +0300

    insertion sort implementation

--- Algorithms/Sorting Algorithms/insertionSort.js
+++ Algorithms/Sorting Algorithms/insertionSort.js
@@ -8,4 +8,32 @@
 // if the number being compared is bigger than the other, then you shift the bigger number to the right.
 
 
+function InsertionSort(arr){
 
+    // we start from index 1 because we assume that the first element of the array is already sorted.
+    // the for loop itarates over the unsorted part of the array
+    for(var i = 1; i < arr.length; i++){
+
+        // this number to be inserted needs to be compared with each element of the sorted part.
+        let numberToInsert = arr[i];
+        // j refers to the index of the sorted element
+        let j = i -1
+        // the sorted elements can be more than one, we need logic to iterate over the sorted parts of the elements
+
+        // in this condition check if the sorted element is greater than the element to insert, otherwise, there is no need to iterate
+        while(j >= 0 && arr[j] > numberToInsert){
+            arr[j +1] = arr[j];
+            j = j-1
+        }
+        arr[j+1] = numberToInsert
+
+    }
+
+
+}
+
+const arr = [8,20,-2,4,-6]
+InsertionSort(arr)
+console.log(arr)
+
+// the Big O notation for this is O(n^2) it has a nested loop
