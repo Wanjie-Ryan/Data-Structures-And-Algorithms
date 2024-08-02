b9eb71fb988925860b1053c441fe4e8e00938db2
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-02 15:10:39 +0300

    insertion sort implementation

--- Algorithms/Sorting Algorithms/insertionSort.js
+++ Algorithms/Sorting Algorithms/insertionSort.js
@@ -1,1 +1,11 @@
 // Given an array of integers, sort the array
+// virtually split the array into a sorted and an unsorted part
+// Assume that the first element is already sorted and the remaining elements are unsorted
+// select an unsorted element and compare with all elements in the sorted part
+// if the elements in the sorted part is smaller than the selected element, proceed to the next element in the unsorted part.Else, shift larger elements in the sorted part towards the right.
+// insert the selected element at the right index
+// repeat till all unsorted elements are placed in the right order
+// if the number being compared is bigger than the other, then you shift the bigger number to the right.
+
+
+
