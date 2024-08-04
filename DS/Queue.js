39447b512e9db939bc304fbc59bef551a90196a8
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-04 09:17:32 +0300

    Queue  implementation

--- DS/Queue.js
+++ DS/Queue.js
@@ -6,55 +6,53 @@
 // pop - removes the last element of the array and returns the element
 // push - adds an element at the end of the array and returns the length of the array
 
-class Queue{
+class Queue {
+  // create a constructor to intialize a new list
 
-    // create a constructor to intialize a new list
+  constructor() {
+    this.Queue = [];
+  }
 
-    constructor(){
-        this.Queue = []
+  // implementing method enqueue that adds an element to the end of the queue using the push method
+
+  enqueue(element) {
+    this.Queue.push(element);
+  }
+
+  dequeue() {
+    return this.Queue.shift();
+  }
+
+  isEmpty() {
+    return this.Queue.length === 0;
+  }
+
+  peek() {
+    // if the Queue is not empty then it will return the first element of the queue
+    if (!this.isEmpty()) {
+      return this.Queue[0];
     }
+    return null;
+  }
+  size() {
+    return this.Queue.length;
+  }
 
-    // implementing method enqueue that adds an element to the end of the queue using the push method
-
-    enqueue(element){
-        this.Queue.push(element)
-    }
-
-    dequeue(){
-        return this.Queue.shift()
-    }
-
-    isEmpty(){
-        return this.Queue.length === 0
-    }
-
-    peek(){
-        // if the Queue is not empty then it will return the first element of the queue 
-        if(!this.isEmpty()){
-            return this.Queue[0]
-        }
-        return null
-    }
-    size(){
-        return this.Queue.length
-    }
-
-    print(){
-        console.log(this.Queue.toString())
-    }
-
+  print() {
+    console.log(this.Queue.toString());
+  }
 }
 
 // creating a new instance of the Queue
 
-const queue = new Queue
-console.log(queue.isEmpty()) 
-queue.enqueue(20)
-queue.enqueue(40)
-queue.enqueue(10)
-queue.enqueue(60)
-queue.enqueue(80)
-console.log(queue.size())
-queue.print()
-console.log(queue.dequeue())
-console.log(queue.peek())
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue(20);
+queue.enqueue(40);
+queue.enqueue(10);
+queue.enqueue(60);
+queue.enqueue(80);
+console.log(queue.size());
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
