c94c802bd8ab20e59b651f01011fa406aa86a48e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-04 11:27:24 +0300

    LinkedList  implementation

---
+++ DS/LinkedList.js
@@ -0,0 +1,7 @@
+// linear data structure that includes a series of connected nodes
+// each node consists of a data value and a pointer that points to the next node
+// the list elements can be easily inserted or removed without reallocation or reorganization of the entire structure
+// random access of elements is not feasible
+// main ops in a linked list - 1. Insertion - add an element at the beginning, end or at a given index in the list. 2. Deletion - remove an item given its index or value. 3 search - find an element given its value
+// linked lists are used to implement stacks and queues
+//  (head)10 -> 20 -> 30 -> null
