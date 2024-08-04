1e3dd66f478b5bd01b6f824f3e3cd5c8a16879fb
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-04 08:59:31 +0300

    Queue  implementation

---
+++ DS/Queue.js
@@ -0,0 +1,17 @@
+// follows the principle of First in first out (FIFO).............. Queue of people in a line
+// Queue data structure supports two main ops: 1. Enqueue - adds an element at the tail of the collection. 2. Dequeue - removes an element from the head of the collection.
+// apart from enqueue and dequeue, there is also 1. Peek - gets the value of element at the front of the queue without removing it. 2. isEmpty - checks if the queue is empty. 3. size - gets the number of elements in the queue. 4. print - visualize the elements in the queue.
+// shift - removes the first element from an array and returns the element
+// unshift - adds a new element at the beginning of the array and return the new length of the array.
+// pop - removes the last element of the array and returns the element
+// push - adds an element at the end of the array and returns the length of the array
+
+class Queue{
+
+    // create a constructor to intialize a new list
+
+    constructor(){
+        this.Queue = []
+    }
+
+}
