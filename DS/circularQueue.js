c3b1c715a9581b298a4a493cba254193425089bf
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-04 09:28:17 +0300

    deQueue  implementation

---
+++ DS/circularQueue.js
@@ -0,0 +1,3 @@
+// the size of the is fixed and a single block of memeory is used as if the first element is connected to the last element. Also referred to as circular buffer or ring buffer and follows the FIFO principle.
+// A cicular queue will reuse the empty block created during the dequeue operation. When working with queues of fixed max size, a circular queue is a great implementation choice.
+// supports 2 main ops: 1. Enqueue, 2. Dequeue.
